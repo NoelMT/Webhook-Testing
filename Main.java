3eb79c2b5a573b4f24788c45056886ec97f618a7
Noel Tesfalidet <Noelt@kth.se>
2024-02-11 16:37:12 +0100

    nother one

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// Version: 1.0 used mainly for testing purposes and to check if the environment is set up correctly
+// Version: 1.0 used mainly for testing purposes and to check if the environment is set up correctly for Java development.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
