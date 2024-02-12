c1db037e6ee5ce08f21dfe6c7ae5ccf48ee7829d
Noel Tesfalidet <Noelt@kth.se>
2024-02-12 12:32:06 +0100

    nother one

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 // Version: 1.0 used mainly for testing purposes and to check if the environment is set up correctly for Java development.
 // This file is part testing if webhook
 // This file is part of the Java Development Environment setup for the project. 
-// shoudl be maven
+// shoudl be maven strucutre
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
